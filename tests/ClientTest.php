1c86c3c887ffeca709abd2caebad2c6ddeaa7ebe
clayton <claytonccollins@gmail.com>
2017-02-24 10:09:11 -0800

    fail client find test

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -74,7 +74,27 @@
 
             //assert
             $this->assertEquals($result, []);
+        }
 
+        function test_find() {
+            //arrange
+            $name = 'sarah is god';
+            $stylist_id = 2;
+            $id = null;
+            $test_client = new Client($name,$stylist_id, $id);
+
+            $name2 = 'shrek is life';
+            $stylist_id2 = 3;
+            $id2 = null;
+            $test_client2 = new Client($name2, $stylist_id2, $id2);
+
+            //act
+            $test_client->save();
+            $test_client2->save();
+            $result = Client::find($test_client2->getId());
+
+            //assert
+            $this->assertEquals($result[0], $test_client2);
         }
     }
 ?>
